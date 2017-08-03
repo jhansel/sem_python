846326489ad528be582d5748b2c637c91f8ade23
hansje <joshua.hansel@inl.gov>
2017-08-03 13:11:21 -0600

    Fixed Berry pI_bar

--- src/aux/BerryInterfacePressureBar.py
+++ src/aux/BerryInterfacePressureBar.py
@@ -19,25 +19,25 @@ class BerryInterfacePressureBar(AuxQuantity2Phase):
     z1 = data["z1"]
     z2 = data["z2"]
 
-    numerator = (z1 * p1 + z2 * p2)
+    numerator = (z1 * p2 + z2 * p1)
     denominator = z1 + z2
     data["pI_bar"] = numerator / denominator
 
     dnumerator = 1.0 / denominator
     ddenominator = -numerator / denominator**2
-    dvf1 = dnumerator * (der["z1"]["vf1"] * p1 + z1 * der["p1"]["vf1"] + der["z2"]["vf1"] * p2 + z2 * der["p2"]["vf1"]) \
+    dvf1 = dnumerator * (der["z1"]["vf1"] * p2 + z1 * der["p2"]["vf1"] + der["z2"]["vf1"] * p1 + z2 * der["p1"]["vf1"]) \
       + ddenominator * (der["z1"]["vf1"] + der["z2"]["vf1"])
-    darho1 = dnumerator * (der["z1"]["arho1"] * p1 + z1 * der["p1"]["arho1"]) \
+    darho1 = dnumerator * (der["z1"]["arho1"] * p2 + z2 * der["p1"]["arho1"]) \
       + ddenominator * der["z1"]["arho1"]
-    darhou1 = dnumerator * (der["z1"]["arhou1"] * p1 + z1 * der["p1"]["arhou1"]) \
+    darhou1 = dnumerator * (der["z1"]["arhou1"] * p2 + z2 * der["p1"]["arhou1"]) \
       + ddenominator * der["z1"]["arhou1"]
-    darhoE1 = dnumerator * (der["z1"]["arhoE1"] * p1 + z1 * der["p1"]["arhoE1"]) \
+    darhoE1 = dnumerator * (der["z1"]["arhoE1"] * p2 + z2 * der["p1"]["arhoE1"]) \
       + ddenominator * der["z1"]["arhoE1"]
-    darho2 = dnumerator * (der["z2"]["arho2"] * p2 + z2 * der["p2"]["arho2"]) \
+    darho2 = dnumerator * (z1 * der["p2"]["arho2"] + der["z2"]["arho2"] * p1) \
       + ddenominator * der["z2"]["arho2"]
-    darhou2 = dnumerator * (der["z2"]["arhou2"] * p2 + z2 * der["p2"]["arhou2"]) \
+    darhou2 = dnumerator * (z1 * der["p2"]["arhou2"] + der["z2"]["arhou2"] * p1) \
       + ddenominator * der["z2"]["arhou2"]
-    darhoE2 = dnumerator * (der["z2"]["arhoE2"] * p2 + z2 * der["p2"]["arhoE2"]) \
+    darhoE2 = dnumerator * (z1 * der["p2"]["arhoE2"] + der["z2"]["arhoE2"] * p1) \
       + ddenominator * der["z2"]["arhoE2"]
 
     der["pI_bar"] = {"vf1": dvf1, "arho1": darho1, "arhou1": darhou1, "arhoE1": darhoE1,
