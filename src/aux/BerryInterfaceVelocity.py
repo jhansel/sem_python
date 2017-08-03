3d955a90c21c24b84624b8380f7335c81ff57dab
hansje <joshua.hansel@inl.gov>
2017-08-03 09:27:24 -0600

    Added Berry interface velocity aux

---
+++ src/aux/BerryInterfaceVelocity.py
@@ -0,0 +1,42 @@
+from numpy import sign
+
+import os
+import sys
+base_dir = os.environ["SEM_PYTHON_DIR"]
+
+sys.path.append(base_dir + "src/aux")
+from AuxQuantity2Phase import AuxQuantity2Phase, AuxQuantity2PhaseParameters
+
+class BerryInterfaceVelocityParameters(AuxQuantity2PhaseParameters):
+  def __init__(self):
+    AuxQuantity2PhaseParameters.__init__(self)
+
+class BerryInterfaceVelocity(AuxQuantity2Phase):
+  def __init__(self, params):
+    AuxQuantity2Phase.__init__(self, params)
+
+  def compute(self, data, der):
+    u_int_bar = data["u_int_bar"]
+    grad_vf1 = data["grad_vf1"]
+    p1 = data["p1"]
+    p2 = data["p2"]
+    z1 = data["z1"]
+    z2 = data["z2"]
+
+    numerator = sign(grad_vf1) * (p2 - p1)
+    denominator = z1 + z2
+    data["u_int"] = u_int_bar + numerator / denominator
+
+    dnumerator = 1.0 / denominator
+    ddenominator = -numerator / denominator**2
+    dvf1 = der["u_int_bar"]["vf1"] + dnumerator * (der["p2"]["vf1"] - der["p1"]["vf1"]) \
+      + ddenominator * (der["z1"]["vf1"] + der["z2"]["vf1"])
+    darho1 = der["u_int_bar"]["arho1"] - dnumerator * der["p1"]["arho1"] + ddenominator * der["z1"]["arho1"]
+    darhou1 = der["u_int_bar"]["arhou1"] - dnumerator * der["p1"]["arhou1"] + ddenominator * der["z1"]["arhou1"]
+    darhoE1 = der["u_int_bar"]["arhoE1"] - dnumerator * der["p1"]["arhoE1"] + ddenominator * der["z1"]["arhoE1"]
+    darho2 = der["u_int_bar"]["arho2"] + dnumerator * der["p2"]["arho2"] + ddenominator * der["z2"]["arho2"]
+    darhou2 = der["u_int_bar"]["arhou2"] + dnumerator * der["p2"]["arhou2"] + ddenominator * der["z2"]["arhou2"]
+    darhoE2 = der["u_int_bar"]["arhoE2"] + dnumerator * der["p2"]["arhoE2"] + ddenominator * der["z2"]["arhoE2"]
+
+    der["u_int"] = {"vf1": dvf1, "arho1": darho1, "arhou1": darhou1, "arhoE1": darhoE1,
+      "arho2": darho2, "arhou2": darhou2, "arhoE2": darhoE2}
---
+++ testing/tests/derivatives/aux/BerryInterfaceVelocityDerivativesTester.py
@@ -0,0 +1,72 @@
+import os
+import sys
+base_dir = os.environ["SEM_PYTHON_DIR"]
+
+import unittest
+
+sys.path.append(base_dir + "src/aux")
+from BerryInterfaceVelocity import BerryInterfaceVelocity, BerryInterfaceVelocityParameters
+from TestAux import TestAux, TestAuxParameters
+
+sys.path.append(base_dir + "testing/src/utilities")
+from AuxDerivativesTester import AuxDerivativesTester
+
+# test aux
+params = BerryInterfaceVelocityParameters()
+test_aux = BerryInterfaceVelocity(params)
+test_var = "u_int"
+
+# bar interface velocity aux
+params = TestAuxParameters()
+params.set("var", "u_int_bar")
+params.set("other_vars", ["vf1", "arho1", "arhou1", "arhoE1", "arho2", "arhou2", "arhoE2"])
+params.set("coefs", [1.1, 1.2, 1.3, 1.4, 1.5, 1.6, 1.7])
+u_int_bar_aux = TestAux(params)
+
+# phase-1 pressure aux
+params = TestAuxParameters()
+params.set("var", "p1")
+params.set("other_vars", ["vf1", "arho1", "arhou1", "arhoE1"])
+params.set("coefs", [2.1, 2.3, 3.4, 4.5])
+p1_aux = TestAux(params)
+
+# phase-2 pressure aux
+params = TestAuxParameters()
+params.set("var", "p2")
+params.set("other_vars", ["vf1", "arho2", "arhou2", "arhoE2"])
+params.set("coefs", [2.4, 2.2, 3.3, 4.4])
+p2_aux = TestAux(params)
+
+# phase-1 acoustic impedance aux
+params = TestAuxParameters()
+params.set("var", "z1")
+params.set("other_vars", ["vf1", "arho1", "arhou1", "arhoE1"])
+params.set("coefs", [1.6, 2.3, 4.5, 2.1])
+z1_aux = TestAux(params)
+
+# phase-2 acoustic impedance aux
+params = TestAuxParameters()
+params.set("var", "z2")
+params.set("other_vars", ["vf1", "arho2", "arhou2", "arhoE2"])
+params.set("coefs", [1.2, 3.2, 4.1, 2.4])
+z2_aux = TestAux(params)
+
+other_aux = {"u_int_bar": u_int_bar_aux, "p1": p1_aux, "p2": p2_aux, "z1": z1_aux, "z2": z2_aux}
+other_vars = ["u_int_bar", "p1", "p2", "z1", "z2"]
+root_vars = ["vf1", "arho1", "arhou1", "arhoE1", "arho2", "arhou2", "arhoE2"]
+constant_data = {"grad_vf1": 0.6}
+
+class BerryInterfaceVelocityDerivativesTester(unittest.TestCase):
+  def setUp(self):
+    self.derivatives_tester = AuxDerivativesTester()
+
+  def test(self):
+    rel_diffs = self.derivatives_tester.checkDerivatives(
+      test_aux, test_var, other_aux, other_vars, root_vars, constant_data)
+    for key in rel_diffs:
+      self.assertLessEqual(rel_diffs[key], 1e-6)
+
+if __name__ == "__main__":
+  derivatives_tester = AuxDerivativesTester(True)
+  _ = derivatives_tester.checkDerivatives(
+    test_aux, test_var, other_aux, other_vars, root_vars, constant_data)
