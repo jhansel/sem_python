35a58210ac9746465d6bbdaae0df777360506f09
Joshua Hansel <joshua.hansel89@gmail.com>
2017-08-20 15:12:36 -0600

    Added a constant aux

---
+++ testing/tests/derivatives/aux/ConstantAuxDerivativesTester.py
@@ -0,0 +1,27 @@
+import unittest
+
+from ConstantAux import ConstantAux, ConstantAuxParameters
+from TestAux import TestAux, TestAuxParameters
+from AuxDerivativesTester import AuxDerivativesTester
+
+# test aux
+params = ConstantAuxParameters()
+params.set("name", "test_aux")
+params.set("value", 6.0)
+test_aux = ConstantAux(params)
+
+other_aux = list()
+root_vars = ["vf1", "arho1", "arhou1", "arhoE1", "arho2", "arhou2", "arhoE2"]
+
+class ConstantAuxDerivativesTester(unittest.TestCase):
+  def setUp(self):
+    self.derivatives_tester = AuxDerivativesTester()
+
+  def test(self):
+    rel_diffs = self.derivatives_tester.checkDerivatives(test_aux, other_aux, root_vars)
+    for key in rel_diffs:
+      self.assertLessEqual(rel_diffs[key], 1e-6)
+
+if __name__ == "__main__":
+  derivatives_tester = AuxDerivativesTester(True)
+  _ = derivatives_tester.checkDerivatives(test_aux, other_aux, root_vars)
