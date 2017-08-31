85e17e6012255f5d84206c26c7ba83a55fc29744
Joshua Hansel <joshua.hansel@inl.gov>
2017-08-31 13:51:45 -0600

    Added output captor and test for display DoF vector

---
+++ testing/tests/utilities/DisplayUtilitiesTester.py
@@ -0,0 +1,36 @@
+from filecmp import cmp
+import sys
+import unittest
+
+from display_utilities import printDoFVector
+from Factory import Factory
+from OutputCaptor import OutputCaptor
+
+def outputPrintDoFVectorToFile(path):
+  # create the DoF handler
+  factory = Factory()
+  n_cell = 5
+  mesh = factory.createObject("UniformMesh", {"name": "mesh", "n_cell": n_cell})
+  dof_handler = factory.createObject("DoFHandler1Phase", {"meshes": [mesh]})
+
+  # solution vector
+  U = range((n_cell + 1)*3)
+
+  # capture the output
+  captor = OutputCaptor()
+  printDoFVector(U, dof_handler)
+  out = captor.getCapturedOutput()
+
+  # write the output file
+  text_file = open(path + "print_dof_vector.txt", "w")
+  text_file.write(out)
+  text_file.close()
+
+class DisplayUtilitiesTester(unittest.TestCase):
+  def testPrintDoFVector(self):
+    path = "tests/utilities/"
+    outputPrintDoFVectorToFile(path)
+    self.assertTrue(cmp(path + "print_dof_vector.txt", path + "gold/print_dof_vector.txt"))
+
+if __name__ == "__main__":
+  outputPrintDoFVectorToFile("")
