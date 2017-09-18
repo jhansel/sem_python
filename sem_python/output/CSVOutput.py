5c17fb9f996a797cc6af992e24258516e2d3c6ec
Joshua Hansel <joshua.hansel@inl.gov>
2017-09-18 09:55:04 -0600

    Added ability to save CSV output by mesh

--- sem_python/output/CSVOutput.py
+++ sem_python/output/CSVOutput.py
@@ -9,6 +9,7 @@ class CSVOutputParameters(OutputParameters):
     self.registerStringParameter("file_name", "Name of solution output file", "solution.csv")
     self.registerStringListParameter("names", "List of names of quantities to save")
     self.registerIntParameter("output_precision", "Precision used in solution output file", 5)
+    self.registerBoolParameter("save_by_mesh", "Flag to save data in separate output files for each mesh", False)
 
 class CSVOutput(Output):
   def __init__(self, params):
@@ -16,11 +17,30 @@ class CSVOutput(Output):
     self.file_name = params.get("file_name")
     self.names = params.get("names")
     self.output_precision = params.get("output_precision")
+    self.save_by_mesh = params.get("save_by_mesh")
 
   def run(self, data):
-    save_data = OrderedDict()
-    save_data["x"] = data["x"]
-    for name in self.names:
-      save_data[name] = data[name]
+    if self.save_by_mesh:
+      save_data = list()
+      for i_mesh in xrange(self.dof_handler.n_meshes):
+        save_data.append(OrderedDict())
 
-    writeCSVFile(save_data, self.file_name, self.output_precision)
+      x = self.dof_handler.separateNodalQuantityByMesh(data["x"])
+      for i_mesh in xrange(self.dof_handler.n_meshes):
+        save_data[i_mesh]["x"] = x[i_mesh]
+
+      for name in self.names:
+        data_name = self.dof_handler.separateNodalQuantityByMesh(data[name])
+        for i_mesh in xrange(self.dof_handler.n_meshes):
+          save_data[i_mesh][name] = data_name[i_mesh]
+
+      for i_mesh in xrange(self.dof_handler.n_meshes):
+        mesh_file_name = self.file_name.rstrip(".csv") + "_" + str(i_mesh+1) + ".csv"
+        writeCSVFile(save_data[i_mesh], mesh_file_name, self.output_precision)
+    else:
+      save_data = OrderedDict()
+      save_data["x"] = data["x"]
+      for name in self.names:
+        save_data[name] = data[name]
+
+      writeCSVFile(save_data, self.file_name, self.output_precision)
