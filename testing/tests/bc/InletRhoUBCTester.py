24a7b5ac6795e75c7e7c0bd8138f1970ef847958
Joshua Hansel <joshua.hansel89@gmail.com>
2017-09-23 15:21:02 -0600

    Added tester for InletRhoUBC

---
+++ testing/tests/bc/InletRhoUBCTester.py
@@ -0,0 +1,20 @@
+import unittest
+
+from BCTester import BCTester
+
+bc_params={"phase": "0", "rho": "1.2", "u": "0.6"}
+
+class InletRhoUBCTester(unittest.TestCase):
+  def setUp(self):
+    self.tester = BCTester()
+
+  def testJacobian(self):
+    rel_diffs = self.tester.checkJacobian("InletRhoUBC", bc_params=bc_params)
+    n_i, n_j = rel_diffs.shape
+    for i in xrange(n_i):
+      for j in xrange(n_j):
+        self.assertLessEqual(rel_diffs[i,j], 1e-6)
+
+if __name__ == "__main__":
+  tester = BCTester(True)
+  _ = tester.checkJacobian("InletRhoUBC", bc_params=bc_params)
