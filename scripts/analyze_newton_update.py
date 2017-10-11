0910c7a1521a8c664c16b9223d7a6e9b88ea0942
Joshua Hansel <joshua.hansel@inl.gov>
2017-10-11 11:11:17 -0600

    Added some scripts

---
+++ scripts/analyze_newton_update.py
@@ -0,0 +1,34 @@
+"""
+This script analyzes an entry of a Newton update vector to determine why it
+attained an unphysical value.
+
+Usage:
+  analyze_newton_update.py -h | --help
+  analyze_newton_update.py
+
+Options:
+  -h --help  Display help information.
+"""
+
+from docopt import docopt
+import numpy as np
+
+docopt(__doc__)
+
+i = 1
+
+J = np.loadtxt("jacobian.csv", delimiter=",")
+r = np.loadtxt("residual.csv", delimiter=",")
+n = J.shape[0]
+
+J_inv = np.linalg.inv(J)
+dU = J_inv.dot(-r)
+
+print "norm(J) = %e" % (np.linalg.cond(J))
+
+print ""
+
+print "%4s: %12s %12s %12s" % ("i", "Jinv[i,j]", "-r[j]", "-Jinvr[i,j]")
+print 44 * "="
+for j in xrange(n):
+  print "%4d: %12.2e %12.2e %12.2e" % (j, J_inv[i,j], -r[j], J_inv[i,j] * -r[j])
---
+++ scripts/print_matrix_rows.py
@@ -0,0 +1,45 @@
+"""
+This script prints a set of rows from a matrix.
+
+Usage:
+  print_matrix_rows.py -h | --help
+  print_matrix_rows.py
+
+Options:
+  -h --help  Display help information.
+"""
+
+from copy import deepcopy
+from docopt import docopt
+import numpy as np
+from numpy.linalg import matrix_rank
+from sys import exit
+
+docopt(__doc__)
+
+A = np.loadtxt("jacobian.csv", delimiter=",")
+i_list = [63, 64, 65]
+
+n = A.shape[0]
+
+# print out entries of rows
+j_min_nonzero = n
+j_max_nonzero = 0
+for i in i_list:
+  # find minimum j that is nonzero
+  row_is_zero = True
+  for j in xrange(n):
+    if abs(A[i,j]) > 1e-14:
+      row_is_zero = False
+      j_min_nonzero = min(j_min_nonzero, j)
+      j_max_nonzero = max(j_max_nonzero, j)
+  if row_is_zero:
+    print "Row %d is a zero row." % (i)
+
+print "Nonzero range: j in (%d, %d)" % (j_min_nonzero, j_max_nonzero)
+
+print "Nonzeros for each selected row:"
+n_entries = j_max_nonzero - j_min_nonzero + 1
+line_format = "%10.2e" * n_entries
+for i in i_list:
+  print line_format % tuple(A[i,j_min_nonzero:j_max_nonzero+1])
