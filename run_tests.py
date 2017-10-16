8bec3824508e8c4b8de161f65cde5c9b6a4c7d04
Joshua Hansel <joshua.hansel@inl.gov>
2017-10-16 07:46:52 -0600

    Added test filter capability

--- run_tests.py
+++ run_tests.py
@@ -1,3 +1,17 @@
+"""
+This script runs the test suite or a subset of the test suite.
+
+Usage:
+  run_tests.py -h | --help
+  run_tests.py [<REGEX>]
+
+Options:
+  -h --help  Display help information.
+
+Arguments:
+  REGEX  Regular expression for test modules to include
+"""
+
 # To get test results in color, install "colour-runner":
 #   pip install colour-runner
 try:
@@ -7,15 +21,26 @@ except:
   from unittest import TextTestRunner
   print_in_color = False
 
+from docopt import docopt
 import os
 import re
 import sys
 from unittest import defaultTestLoader
 
 def main():
+  # parse command-line arguments using docopt
+  command_line_arguments = docopt(__doc__)
+
+  # get the the regex parameter, if any
+  regex = command_line_arguments["<REGEX>"]
+  if regex:
+    search_pattern = "*" + regex + "*Tester.py"
+  else:
+    search_pattern = "*Tester.py"
+
   # add tests to test suite
   suite = defaultTestLoader.discover(
-    start_dir='testing/tests', pattern='*Tester.py', top_level_dir='.')
+    start_dir='testing/tests', pattern=search_pattern, top_level_dir='.')
 
   # run test suite
   if print_in_color:
