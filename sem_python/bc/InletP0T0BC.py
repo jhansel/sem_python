fd830d90a0edaae29afde18dbcbbdd1407f142a8
Joshua Hansel <joshua.hansel@inl.gov>
2017-10-18 09:16:50 -0600

    Added strong options to InletP0T0BC

--- sem_python/bc/InletP0T0BC.py
+++ sem_python/bc/InletP0T0BC.py
@@ -8,6 +8,8 @@ class InletP0T0BCParameters(OnePhaseBCParameters):
     OnePhaseBCParameters.__init__(self)
     self.registerFloatParameter("p0", "Specified stagnation pressure")
     self.registerFloatParameter("T0", "Specified stagnation temperature")
+    self.registerBoolParameter("strongly_enforce_mass", "Strongly enforce mass?", True)
+    self.registerBoolParameter("strongly_enforce_momentum", "Strongly enforce momentum?", False)
     self.registerBoolParameter("strongly_enforce_energy", "Strongly enforce energy?", False)
 
 class InletP0T0BC(OnePhaseBC):
@@ -15,6 +17,8 @@ class InletP0T0BC(OnePhaseBC):
     OnePhaseBC.__init__(self, params)
     self.p0 = params.get("p0")
     self.T0 = params.get("T0")
+    self.strongly_enforce_mass = params.get("strongly_enforce_mass")
+    self.strongly_enforce_momentum = params.get("strongly_enforce_momentum")
     self.strongly_enforce_energy = params.get("strongly_enforce_energy")
 
   def applyWeakBC(self, U, r, J):
@@ -24,12 +28,21 @@ class InletP0T0BC(OnePhaseBC):
     aA1 = self.dof_handler.aA1(U, self.k)
     vf, dvf_daA1 = computeVolumeFraction(aA1, A, self.phase, self.model_type)
 
+    # mass
+    if not self.strongly_enforce_mass:
+      r[self.i_arhoA] += vf * self.rho * self.u * A * self.nx
+      if self.model_type == ModelType.TwoPhase:
+        J[self.i_arhoA, self.i_aA1] += dvf_daA1 * self.rho * self.u * A * self.nx
+      J[self.i_arhoA, self.i_arhoA] += vf * (self.drho_darhoA * self.u + self.rho * self.du_darhoA) * A * self.nx
+      J[self.i_arhoA, self.i_arhouA] += vf * (self.drho_darhouA * self.u + self.rho * self.du_darhouA) * A * self.nx
+
     # momentum
-    r[self.i_arhouA] += vf * (self.rho * self.u**2 + self.p) * A * self.nx
-    if (self.model_type == ModelType.TwoPhase):
-      J[self.i_arhouA,self.i_aA1] += dvf_daA1 * (self.rho * self.u**2 + self.p) * A * self.nx
-    J[self.i_arhouA,self.i_arhoA]  += vf * (self.drho_darhoA  * self.u**2 + self.rho * 2 * self.u * self.du_darhoA  + self.dp_darhoA)  * A * self.nx
-    J[self.i_arhouA,self.i_arhouA] += vf * (self.drho_darhouA * self.u**2 + self.rho * 2 * self.u * self.du_darhouA + self.dp_darhouA) * A * self.nx
+    if not self.strongly_enforce_momentum:
+      r[self.i_arhouA] += vf * (self.rho * self.u**2 + self.p) * A * self.nx
+      if (self.model_type == ModelType.TwoPhase):
+        J[self.i_arhouA,self.i_aA1] += dvf_daA1 * (self.rho * self.u**2 + self.p) * A * self.nx
+      J[self.i_arhouA,self.i_arhoA]  += vf * (self.drho_darhoA  * self.u**2 + self.rho * 2 * self.u * self.du_darhoA  + self.dp_darhoA)  * A * self.nx
+      J[self.i_arhouA,self.i_arhouA] += vf * (self.drho_darhouA * self.u**2 + self.rho * 2 * self.u * self.du_darhouA + self.dp_darhouA) * A * self.nx
 
     # energy
     if not self.strongly_enforce_energy:
@@ -45,20 +58,38 @@ class InletP0T0BC(OnePhaseBC):
     A = self.dof_handler.A[self.k]
     aA1 = self.dof_handler.aA1(U, self.k)
     vf, dvf_daA1 = computeVolumeFraction(aA1, A, self.phase, self.model_type)
-    arhoA = U[self.i_arhoA]
-
-    arhoABC = vf * self.rho * A
-    darhoABC_daA1 = dvf_daA1 * self.rho * A
-    darhoABC_darhoA  = vf * self.drho_darhoA  * A
-    darhoABC_darhouA = vf * self.drho_darhouA * A
 
     # mass
-    r[self.i_arhoA] = arhoA - arhoABC
-    J[self.i_arhoA,:] = 0
-    if (self.model_type == ModelType.TwoPhase):
-      J[self.i_arhoA,self.i_aA1] = -darhoABC_daA1
-    J[self.i_arhoA,self.i_arhoA] = 1 - darhoABC_darhoA
-    J[self.i_arhoA,self.i_arhouA] = -darhoABC_darhouA
+    if self.strongly_enforce_mass:
+      arhoA = U[self.i_arhoA]
+
+      arhoABC = vf * self.rho * A
+      darhoABC_daA1 = dvf_daA1 * self.rho * A
+      darhoABC_darhoA  = vf * self.drho_darhoA  * A
+      darhoABC_darhouA = vf * self.drho_darhouA * A
+
+      r[self.i_arhoA] = arhoA - arhoABC
+      J[self.i_arhoA,:] = 0
+      if (self.model_type == ModelType.TwoPhase):
+        J[self.i_arhoA,self.i_aA1] = -darhoABC_daA1
+      J[self.i_arhoA,self.i_arhoA] = 1 - darhoABC_darhoA
+      J[self.i_arhoA,self.i_arhouA] = -darhoABC_darhouA
+
+    # momentum
+    if self.strongly_enforce_momentum:
+      arhouA = U[self.i_arhouA]
+
+      arhouABC = vf * (self.rho * self.u**2 + self.p) * A
+      darhouABC_daA1 = dvf_daA1 * (self.rho * self.u**2 + self.p) * A
+      darhouABC_darhoA = vf * (self.drho_darhoA * self.u**2 + self.rho * 2 * self.u * self.du_darhoA + self.dp_darhoA) * A
+      darhouABC_darhouA = vf * (self.drho_darhouA * self.u**2 + self.rho * 2 * self.u * self.du_darhouA + self.dp_darhouA) * A
+
+      r[self.i_arhouA] = arhouA - arhouABC
+      J[self.i_arhouA,:] = 0
+      if self.model_type == ModelType.TwoPhase:
+        J[self.i_arhouA, self.i_aA1] = -darhouABC_daA1
+      J[self.i_arhouA, self.i_arhoA]  = -darhouABC_darhoA
+      J[self.i_arhouA, self.i_arhouA] = -darhouABC_darhouA
 
     # energy
     if self.strongly_enforce_energy:
--- testing/tests/bc/InletP0T0BCTester.py
+++ testing/tests/bc/InletP0T0BCTester.py
@@ -7,14 +7,16 @@ class InletP0T0BCTester(unittest.TestCase):
     self.tester = BCTester()
 
   def testJacobianWeaklyEnforce(self):
-    rel_diffs = self.tester.checkJacobian("InletP0T0BC", bc_params={"phase": "0", "p0": "0.2", "T0": "0.6", "strongly_enforce_energy": False})
+    rel_diffs = self.tester.checkJacobian("InletP0T0BC", bc_params={"phase": "0", "p0": "0.2", "T0": "0.6",
+      "strongly_enforce_mass": False, "strongly_enforce_momentum": False, "strongly_enforce_energy": False})
     n_i, n_j = rel_diffs.shape
     for i in xrange(n_i):
       for j in xrange(n_j):
         self.assertLessEqual(rel_diffs[i,j], 1e-6)
 
   def testJacobianStronglyEnforce(self):
-    rel_diffs = self.tester.checkJacobian("InletP0T0BC", bc_params={"phase": "0", "p0": "0.2", "T0": "0.6", "strongly_enforce_energy": True})
+    rel_diffs = self.tester.checkJacobian("InletP0T0BC", bc_params={"phase": "0", "p0": "0.2", "T0": "0.6",
+      "strongly_enforce_mass": True, "strongly_enforce_momentum": True, "strongly_enforce_energy": True})
     n_i, n_j = rel_diffs.shape
     for i in xrange(n_i):
       for j in xrange(n_j):
